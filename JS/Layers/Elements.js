cf9893de26575eef88bd98eeda2cde4c75956f7a
Genoplex <58967783+Genoplex@users.noreply.github.com>
2021-01-31 21:20:27 +0800

    Add files via upload

---
+++ JS/Layers/Elements.js
@@ -0,0 +1,396 @@
+addLayer("Elements",
+{
+    name         : "Elements",
+    symbol       : "E",
+    resource     : "Atoms",
+    baseResource : "Particles",
+    color        : "#F8FFB8",
+    type         : "normal",
+    exponent     : 1,
+    position     : 1,
+    row          : 1,
+    requires     : new Decimal(1000),
+    branches     : ["Particles"],
+    base         : 1000,
+
+    resetDescription : "Merge Particles for ",
+
+    tabFormat:
+    [
+        "main-display",
+        "prestige-button",
+        "blank",
+        ["display-text",function(){return 'You have ' + format(player.Particles.points) + ' Particles'}],
+        "blank",
+        ["display-text",function(){return 'You have ' + format(player.Elements.points) + ' Atoms and they boost production by ' + format(tmp["Elements"].effect) + "x" }],
+        "blank",
+        "buyables",
+        "blank",
+        "upgrades",
+        "blank",
+        "milestones",
+        "blank",
+    ],
+
+
+    doReset()
+    {
+        player.resetTime = 0
+    },
+
+    canBuyMax    : function()
+    {
+        return true
+    },
+
+    layerShown()
+    {
+        return (hasUpgrade("Particles",33) || player[this.layer].unlocked)
+    },
+
+    startData()
+    {    
+        return{
+            unlocked  : false,
+            points    : new Decimal(0),
+            best      : new Decimal(0)
+        }
+    },
+    
+    effect()
+    {
+        multiple = player.Elements.points
+        if (!(hasUpgrade("Elements",41)))
+        {
+            multiple = multiple.pow(0.75).add(1)
+        }
+        if (hasUpgrade("Elements",41))
+        {
+            multiple = multiple.pow(0.8).add(1)
+        }
+        return multiple
+    },
+        
+    baseAmount()
+    {
+        return player.Particles.points
+    },
+        
+    gainMult()
+    {
+        mult = new Decimal(1)
+        return mult
+    },
+
+    gainExp()
+    {
+        return new Decimal(1)
+    },
+
+    update(diff)
+    {
+        player.resetTime += 0.05
+    },
+
+    milestones:
+    {
+        0:
+        {
+            requirementDescription : "Elements Layer Unlocked",
+            effectDescription      : "Atoms begin to boost production",
+            done                   :  function()
+            {
+                return true
+            }
+        },
+        1:
+        {
+            requirementDescription : "E1-1A, E1-1B or E1-1C Bought",
+            effectDescription      : "Generator MK3 save the mechanical components. Keep P1-1, P1-2 and P1-3 on reset.",
+            done                   :  function()
+            {
+                return (hasUpgrade("Elements",11) || hasUpgrade("Elements",12) || hasUpgrade("Elements",13))
+            }
+        },
+        2:
+        {
+            requirementDescription : "100 Atoms in Total",
+            effectDescription      : "Atoms begin to differentiate into elements. Unlock a new Buyable",
+            done                   :  function()
+            {
+                return player[this.layer].best.gte(100)
+            }
+        },
+        3:
+        {
+            requirementDescription : "1,000,000 Atoms in Total",
+            effectDescription      : "Atoms begin to react with each other. Unlock a new layer",
+            done                   :  function()
+            {
+                return player[this.layer].best.gte(10000)
+            }
+        }
+    },
+
+    buyables:
+    {
+        rows:1,
+        cols:1,
+        11:
+        {
+            title : "Number of types of elements",
+            unlocked : function()
+            {
+                return hasMilestone("Elements",2)
+            }
+        }
+    },
+
+    upgrades:
+    {
+        rows:5,
+        cols:5,
+        11:
+        {
+            title       : "E1-1A Generator MK3-Speed",
+            description : "Time resist. Locks B and C",
+            cost()
+            {
+                if((hasUpgrade("Elements",12) && !(hasUpgrade("Elements",51))) || (hasUpgrade("Elements",13) && !(hasUpgrade("Elements",51)) || (hasUpgrade("Elements",12) && hasUpgrade("Elements",13) && hasUpgrade("Elements",51))))
+                {
+                    return Infinity
+                }
+                return 5
+            },
+            effect      :  function()
+            {
+                time = player.resetTime
+                if (!(hasUpgrade("Elements",21)))
+                {
+                    initial = 100
+                }
+                if (hasUpgrade("Elements",21))
+                {
+                    initial = player[this.layer].points.add(1).ln().times(0.1).add(1).times(100)
+                }
+                if (!hasUpgrade("Elements",31))
+                {
+                    reduce = time * 2
+                }
+                if (hasUpgrade("Elements",31))
+                {
+                    reduce_multiple = player[this.layer].points.pow(0.1)
+                    reduce_multiple = 1 / reduce_multiple
+                    if(reduce_multiple > 1 || reduce_multiple < 0)
+                    {
+                        reduce_multiple = 1
+                    }
+                    reduce = time * 2 * reduce_multiple
+                }
+                value = initial - reduce
+                if(value < 0.5)
+                {
+                    value = 0.5
+                }
+                return value
+            }
+        },
+        12:
+        {
+            title       : "E1-1B Generator MK3-Normal",
+            description : "Stable production. Locks A and C",
+            cost()
+            {
+                if((hasUpgrade("Elements",11) && !(hasUpgrade("Elements",51))) || (hasUpgrade("Elements",13) && !(hasUpgrade("Elements",51)) || (hasUpgrade("Elements",11) && hasUpgrade("Elements",13) && hasUpgrade("Elements",51))))
+                {
+                    return Infinity
+                }
+                return 5
+            },
+            effect      :  function()
+            {
+                if (hasUpgrade("Elements",22))
+                {
+                    return 62.5
+                }
+                if (!(hasUpgrade("Elements",22)))
+                {
+                    return 50
+                }
+            }
+        },
+        13:
+        {
+            title       : "E1-1C Generator MK3-Time",
+            description : "Time boosts. Locks A and B",
+            cost()
+            {
+                if((hasUpgrade("Elements",11) && !(hasUpgrade("Elements",51))) || (hasUpgrade("Elements",12) && !(hasUpgrade("Elements",51)) || (hasUpgrade("Elements",11) && hasUpgrade("Elements",12) && hasUpgrade("Elements",51))))
+                {
+                    return Infinity
+                }
+                return 5 
+            },
+            effect      :  function()
+            {
+                time = player.resetTime
+                if (!(hasUpgrade("Elements",23)))
+                {
+                    begin = 1
+                }
+                if (hasUpgrade("Elements",23))
+                {
+                    begin = player[this.layer].points.add(1).ln().times(0.12).add(1)
+                }
+                if (!hasUpgrade("Elements",33))
+                {
+                    increase = time * 2
+                }
+                if (hasUpgrade("Elements",33))
+                {
+                    increase_multiple = player[this.layer].points.pow(0.1)
+                    if(increase_multiple < 1)
+                    {
+                        increase_multiple = 1
+                    }
+                    increase = time * 2 * increase_multiple
+                }
+                boost = increase + begin
+                if(boost < 0.5)
+                {
+                    boost = 0.5
+                }
+                return boost
+            }
+        },
+        21:
+        {
+            title       : "E2-1A Initial speed",
+            description : "Atoms increase the initial speed",
+            cost        :  new Decimal(50),
+            effect      :  function()
+            {
+                initial = player[this.layer].points.add(1).ln().times(0.1).add(1)
+                return initial
+            },
+            unlocked    :  function()
+            {
+                return hasUpgrade("Elements",11)
+            }
+        },
+        22:
+        {
+            title       : "E2-1B Efficient operation",
+            description : "E1-1B is 25% more effective",
+            cost        :  new Decimal(50),
+            unlocked    :  function()
+            {
+                return hasUpgrade("Elements",12)
+            }
+        },
+        23:
+        {
+            title       : "E2-1C ",
+            description : "Atoms increase conduction",
+            cost        :  new Decimal(50),
+            effect      :  function()
+            {
+                begin = player[this.layer].points.add(1).ln().times(0.1).add(1)
+                return begin
+            },
+            unlocked    :  function()
+            {
+                return hasUpgrade("Elements",13)
+            }
+        },
+        31:
+        {
+            title       : "E3-1A Reduce Resistance",
+            description : "Atoms decrease time resistance",
+            cost        :  new Decimal(50),
+            effect      :  function()
+            {
+                reduce_multiple = player[this.layer].points.pow(0.1)
+                reduce_multiple = 1 / reduce_multiple
+                if(reduce_multiple > 1 || reduce_multiple < 0)
+                {
+                    reduce_multiple = 1
+                }
+                return reduce_multiple
+            },
+            unlocked    :  function()
+            {
+                return hasUpgrade("Elements",11)
+            }
+        },
+        32:
+        {
+            title       : "E3-1B Superload",
+            description : "P3-2 is 25% more effective",
+            cost        :  new Decimal(50),
+            unlocked    :  function()
+            {
+                return hasUpgrade("Elements",12)
+            }
+        },
+        33:
+        {
+            title       : "E3-1C Initial Speed",
+            description : "Atoms increase the initial speed",
+            cost        :  new Decimal(50),
+            effect      :  function()
+            {
+                increase_multiple = player[this.layer].points.pow(0.2)
+                if(increase_multiple < 1)
+                {
+                    increase_multiple = 1
+                }
+                return increase_multiple
+            },
+            unlocked    :  function()
+            {
+                return hasUpgrade("Elements",13)
+            }
+        },
+        41:
+        {
+            title       : "E4-1 Fuel Tank",
+            description : "Atom boost use a better formula",
+            cost        :  new Decimal(100),
+            unlocked    :  function()
+            {
+                return ((hasUpgrade("Elements",21) && hasUpgrade("Elements",31)) || (hasUpgrade("Elements",22) && hasUpgrade("Elements",32)) || (hasUpgrade("Elements",23) && hasUpgrade("Elements",33)))
+            }
+        },
+        42:
+        {
+            title       : "E4-2 Auto Fusion",
+            description : "Gain 1% of the Particles gain per second",
+            cost        :  new Decimal(1000),
+            unlocked    :  function()
+            {
+                return (hasUpgrade("Elements",41) && hasUpgrade("Particles",33))
+            }
+        },
+        43:
+        {
+            title       : "E4-3 Component Protection",
+            description : "Keep every P Upgrades on reset",
+            cost        :  new Decimal(10000),
+            unlocked    :  function()
+            {
+                return (hasUpgrade("Elements",42))
+            }
+        },
+        51:
+        {
+            title       : "E5-1 Shell improvement",
+            description : "Enable to buy another type of MK3",
+            cost        :  new Decimal(1e10),
+            unlocked    :  function()
+            {
+                return(hasUpgrade("Elements",11) || hasUpgrade("Elements",12) || hasUpgrade("Elements",13))
+            }
+        }
+    }
+})
---
+++ JS/Layers/Particles.js
@@ -0,0 +1,209 @@
+addLayer("Particles",
+{
+    name         : "Particles",
+    symbol       : "P",
+    resource     : "Particles",
+    baseResource : "Energy",
+    color        : "#FFFFFF",
+    type         : "normal",
+    exponent     : 0.5,
+    position     : 0,
+    row          : 0,
+    requires     : new Decimal(10),
+
+    resetDescription:"Merge Energy for ",
+
+    tabFormat:
+    [
+        "main-display",
+        "prestige-button",
+        "blank",
+        ["display-text",function(){return 'You have ' + format(player.points) + ' Energy'}],
+        "blank",
+        "upgrades",
+    ],
+
+    layerShown()
+    {
+        return true
+    },
+
+    startData()
+    {    
+        return{
+        unlocked: true,
+		points: new Decimal(0),
+        }
+    },
+        
+    baseAmount()
+    {
+        return player.points
+    },
+        
+    gainMult()
+    {
+        mult = new Decimal(1)
+        return mult
+    },
+
+    gainExp()
+    {
+        return new Decimal(1)
+    },
+
+    update(diff)
+    {
+        let gain = getResetGain(this.layer)
+        if (hasUpgrade("Elements",42))
+        {
+            player[this.layer].points = player[this.layer].points.add(gain.times(0.05).times(0.01))
+        }
+    },
+
+    doReset(Layer)
+    {
+        if (Layer && layers[Layer].row > layers[this.layer].row)
+        {
+            var listKeep = [];
+            var upgradeKeep = [];
+            if (Layer === "Elements" && hasMilestone("Elements",1))
+            {
+                upgradeKeep.push(11,21,31)
+            }
+            if (Layer === "Elements" && hasUpgrade("Elements",43))
+            {
+                upgradeKeep.push(11,12,13,21,22,23,31,32,33)
+            }
+            layerDataReset("Particles", listKeep)
+            player.Particles.upgrades = upgradeKeep;
+        }
+    },
+
+    upgrades:
+    {
+        rows:5,
+        cols:5,
+        11:
+        {
+            title       : "P1-1 Generator MK1",
+            description : "Produce 1 Bases per second",
+            cost        :  new Decimal(1)
+        },
+        12:
+        {
+            title       : "P1-2 Charge",
+            description : "Energy boosts production",
+            cost        :  new Decimal(10),
+            effect      :  function()
+            {
+                value = player.points.add(1).log10().add(1)
+                if (hasUpgrade("Particles",22))
+                {
+                    value = player.points.add(1).log10().times(1.5).add(1)
+                }
+                return value
+            },
+            unlocked    :  function()
+            {
+                return hasUpgrade("Particles",11)
+            }
+        },
+        13:
+        {
+            title       : "P1-3 Particle Catalyst",
+            description : "Particles boost production",
+            cost        :  new Decimal(10),
+            effect      :  function()
+            {
+                value = player[this.layer].points.add(1).ln().add(1)
+                if (hasUpgrade("Particles",23))
+                {
+                    value = player[this.layer].points.add(1).ln().times(1.25).add(1)
+                }
+                return value
+            },
+            unlocked    :  function()
+            {
+                return hasUpgrade("Particles",11)
+            }
+        },
+        21:
+        {
+            title       : "P2-1 Generator MK2",
+            description : "Double production",
+            cost        :  new Decimal(25),
+            effect      :  function()
+            {
+                return 2
+            },
+            unlocked    :  function()
+            {
+                return hasUpgrade("Particles",11)
+            }
+        },
+        22:
+        {
+            title       : "P2-2 Battery",
+            description : "P1-2 is 50% more effcient",
+            cost        :  new Decimal(50),
+            unlocked    :  function()
+            {
+                return (hasUpgrade("Particles",12) && hasUpgrade("Particles",21))
+            }
+        },
+        23:
+        {
+            title       : "P2-3 Efficient Catalyst",
+            description : "P1-3 is 25% more effcient",
+            cost        :  new Decimal(50),
+            unlocked    :  function()
+            {
+                return (hasUpgrade("Particles",13) && hasUpgrade("Particles",21))
+            }
+        },
+        31:
+        {
+            title       : "P3-1 Generator MK2+",
+            description : "Triple production",
+            cost        :  new Decimal(100),
+            effect      :  function()
+            {
+                return 3
+            },
+            unlocked    :  function()
+            {
+                return hasUpgrade("Particles",21)
+            }
+        },
+        32:
+        {
+            title       : "P3-2 Overload",
+            description : "Energy and Particles boosts production again",
+            cost        :  new Decimal(250),
+            effect      :  function()
+            {
+                value = player[this.layer].points.times(player.points).add(1).log10().add(1)
+                if(hasUpgrade("Elements",32))
+                {
+                    value = value * 1.25
+                }
+                return value
+            },
+            unlocked    :  function()
+            {
+                return (hasUpgrade("Particles",22) && hasUpgrade("Particles",23) && hasUpgrade("Particles",31))
+            }
+        },
+        33:
+        {
+            title       : "P3-3 Synthesizer",
+            description : "Unlock a new layer",
+            cost        :  new Decimal(1000),
+            unlocked    :  function()
+            {
+                return hasUpgrade("Particles",32)
+            }
+        }
+    }
+})
